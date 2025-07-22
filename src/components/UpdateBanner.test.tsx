8e50d50a5851773909e5205ace667cc4fb06b396
Ville Johannes Pajala <valoraami@gmail.com>
2025-07-22 11:02:24 +0300

    test: silence warnings

--- src/components/UpdateBanner.test.tsx
+++ src/components/UpdateBanner.test.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n.test';
 import UpdateBanner from './UpdateBanner';
 
 describe('UpdateBanner', () => {
   it('renders release notes when provided', () => {
-    render(<UpdateBanner onUpdate={() => {}} notes="Some fixes" />);
+    render(
+      <I18nextProvider i18n={i18n}>
+        <UpdateBanner onUpdate={() => {}} notes="Some fixes" />
+      </I18nextProvider>
+    );
     expect(screen.getByText('Some fixes')).toBeInTheDocument();
   });
 
   it('does not render notes when not provided', () => {
-    render(<UpdateBanner onUpdate={() => {}} />);
+    render(
+      <I18nextProvider i18n={i18n}>
+        <UpdateBanner onUpdate={() => {}} />
+      </I18nextProvider>
+    );
     expect(screen.queryByText('Some fixes')).not.toBeInTheDocument();
   });
 
   it('hides banner when dismissed', () => {
-    render(<UpdateBanner onUpdate={() => {}} />);
+    render(
+      <I18nextProvider i18n={i18n}>
+        <UpdateBanner onUpdate={() => {}} />
+      </I18nextProvider>
+    );
     const button = screen.getByRole('button', { name: /dismiss/i });
     button.click();
     expect(screen.queryByText(/new version/i)).not.toBeInTheDocument();
